a61fe8988f77500e3620b8a1e50e849bad0f61d2
manu <mgonzalez12@SMXDAM.CIDE.ORG>
2019-11-08 20:39:31 +0100

    Ahora volvemos a editar la clase

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@
 class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola món!");
+		public class Main {
+			public static void main(String[] args) {
+			printHola();
+			}
+			public static void printHola(){
+			System.out.println("Hola món!");
+			}
+			}
 
 	}
 
